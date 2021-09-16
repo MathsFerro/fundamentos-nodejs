a0255f53d7390baa25b55801fa70d8fcef0ab03f
Matheus Ferro <matheusferroraimundo@gmail.com>
2021-09-16 14:56:55 -0300

    ajuste comentario

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 
 // 1° Paramêtro: Rota
-// 2° Parametro: {
-// Request está recebendo da requisição
-// Resp vai retornar da nossa requisição }
+/*
+  2° Parametro:
+    Request: está recebendo da requisição
+    Resp vai retornar da nossa requisição 
+*/
 app.get("/", (request, response) => {
   return response.json({
     message: "Hello World!"
